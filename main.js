607a8e3119a194b9617dcf68dc310592c5c9ab2e
github username <lushiyun@outlook.com>
2020-08-02 11:46:07 -0400

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll('.like-glyph');
+const errorModal = document.querySelector('#modal');
 
+document.addEventListener('DOMContentLoaded', loadEventListeners());
 
+function loadEventListeners() {
+  hearts.forEach(heart => heart.addEventListener('click', addLike));
+}
 
+function addLike(e) {
+  mimicServerCall()
+  .then(() => {
+    if(e.target.textContent === EMPTY_HEART) {
+      e.target.textContent = FULL_HEART;
+      e.target.classList.add('activated-heart');
+    } else {
+      e.target.textContent = EMPTY_HEART;
+      e.target.classList.remove('activated-heart');
+    }
+  })
+  .catch(error => {
+    errorModal.textContent = error;
+    errorModal.classList.remove('hidden');
+    setTimeout(() => { errorModal.className = 'hidden'; }, 5000);
+  });
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
