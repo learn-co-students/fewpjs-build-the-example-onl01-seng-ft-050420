fa71970204c8d62c2c5aeaff6ebcee185def13a0
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-08-10 12:08:20 -0400

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorDiv = document.getElementById('modal')
+errorDiv.setAttribute('class', 'hidden')
 
+const heart = document.querySelectorAll(".like-glyph")
+for(let x of heart){
+  x.addEventListener("click", function(){
+    mimicServerCall()
+    .then(() => {
+      if (x.innerHTML === EMPTY_HEART){
+        x.setAttribute('class', 'activated-heart')
+        x.innerHTML = FULL_HEART
+      } else {
+        x.setAttribute("class", '')
+        x.innerHTML = EMPTY_HEART
+      }
+    })
+    .catch(function(error){
+      const pError = document.getElementById('modal-message')
+      pError.innerHTML = error
+      errorDiv.setAttribute('class', '')
+      setTimeout(function(){
+        pError.innerHTML = ""
+        errorDiv.setAttribute('class', 'hidden')
+      }, 5000)
+      console.log(error)
+    })
+  })
+}
 
 
 
